77d2a59134e30e23103699970ef1ec8565ecc0d8
Liam Brenner <liam.brenner@gmail.com>
2023-12-27 23:36:35 +1100

    Re-enable aws adapter tests

--- sendlive/providers/aws/tests/test_aws_adapter.py
+++ sendlive/providers/aws/tests/test_aws_adapter.py
@@ -48,20 +48,18 @@ def sendlive_aws_adapter(sendlive_aws_credentials: AWSCredentials) -> AWSAdapter
     return AWSAdapter(credentials=sendlive_aws_credentials)
 
 
-# def test_aws_adapter_provider_setup(
-#     sendlive_aws_adapter: AWSAdapter,
-# ) -> None:
-#     """Test adapter can successfully attach a boto session to the instance."""
-#     sendlive_aws_adapter.setup_provider()
-#     assert sendlive_aws_adapter._boto_session
+def test_aws_adapter_provider_setup(
+    sendlive_aws_adapter: AWSAdapter,
+) -> None:
+    """Test adapter can successfully attach a boto session to the instance."""
+    assert sendlive_aws_adapter._boto_session
 
 
-# @pytest.fixture(scope="function")
-# def sendlive_aws_adapter_with_boto_session(
-#     sendlive_aws_adapter: AWSAdapter,
-# ) -> AWSAdapter:
-#     sendlive_aws_adapter.setup_provider()
-#     return sendlive_aws_adapter
+@pytest.fixture(scope="function")
+def sendlive_aws_adapter_with_boto_session(
+    sendlive_aws_adapter: AWSAdapter,
+) -> AWSAdapter:
+    return sendlive_aws_adapter
 
 
 def test_aws_adapter_create_stream_with_endpoint_creation(
