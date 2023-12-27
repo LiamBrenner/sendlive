bdd21c940645655dd829a70318eb43e631e17152
Liam Brenner <liam.brenner@gmail.com>
2023-12-28 00:08:59 +1100

    Disable ruff pyupgrade rules in typealias use

--- sendlive/types.py
+++ sendlive/types.py
@@ -3,6 +3,6 @@ from collections.abc import Mapping, Sequence
 
 from typing_extensions import TypeAlias  # noqa: UP035
 
-MappingTags: TypeAlias = Mapping[str, str]
+MappingTags: TypeAlias = Mapping[str, str]  # noqa: UP040
 
-TupleSequenceTags: TypeAlias = Sequence[tuple[str, str]]
+TupleSequenceTags: TypeAlias = Sequence[tuple[str, str]]  # noqa: UP040
