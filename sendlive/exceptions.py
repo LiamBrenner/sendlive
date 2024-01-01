2971f6cd922325047fc4c4928956b2e2886aae8c
Liam Brenner <liam.brenner@gmail.com>
2024-01-02 02:26:02 +1100

    Add cloud provider exception types

--- sendlive/exceptions.py
+++ sendlive/exceptions.py
@@ -1,2 +1,13 @@
 class SendLiveError(Exception):
     """Base exception for all sendlive exceptions."""
+
+
+# Cloud provider related issues
+
+
+class SendLiveProviderError(SendLiveError):
+    """Exception raised when an error occurs related to a cloud provider such as AWS, GCP etc."""
+
+
+class SendLiveProviderPermissionError(SendLiveProviderError):
+    """Exception raised when a permission error is returned by a cloud provider."""
